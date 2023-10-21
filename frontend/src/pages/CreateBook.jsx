b7271a5b1ab82e5f7f4f53a4e6cfc553b2e178d1
SUDHARSAN S V <sudh20331.cs@rmkec.ac.in>
2023-10-21 13:52:53 +0530

    Updated the project with creating, updating and deleting the items

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -8,10 +8,64 @@ const CreateBook = () => {
   const [title,setTitle] = useState('');
   const [author,setAuthor] = useState('');
   const [publishyear,setPublishYear] = useState('');
-  const[loading,setLoading] = useState('');
+  const[loading,setLoading] = useState(false);
   const navigate = useNavigate();
+  const handleSaveBook = () =>{
+    const data = {
+      title,
+      author,
+      publishyear,
+    };
+    setLoading(true);
+    axios
+    .post('http://localhost:5555/books/create',data)
+    .then(() => {
+      setLoading(false);
+      navigate('/');
+    })
+    .catch((error) =>{
+      setLoading(false);
+      alert('An error occured');
+      console.log(error);
+    });
+  };
   return (
-    <div>CreateBook</div>
+    <div className="p-4">
+      <BackButton/>
+      <h1 className="text-3xl my-4">Create Book</h1>
+      {
+        loading ? <Spinner/> : ''
+      }
+      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Title</label>
+          <input 
+          type = 'text'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          className="border-2 border-gray-500 px-4 py-2 w-full"/>
+        </div>
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Author</label>
+          <input 
+          type = 'text'
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+          className="border-2 border-gray-500 px-4 py-2 w-full"/>
+        </div>
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Publish Year</label>
+          <input 
+          type = 'text'
+          value={publishyear}
+          onChange={(e) => setPublishYear(e.target.value)}
+          className="border-2 border-gray-500 px-4 py-2 w-full"/>
+        </div>
+        <button className="p-2 bg-sky-300 m-8" onClick={handleSaveBook}>
+            Save
+        </button>
+      </div>
+    </div>
   )
 }
 
