cf0b3e111d50f5a936fc171643f16af0d75c7ac9
SUDHARSAN S V <sudh20331.cs@rmkec.ac.in>
2023-10-24 19:16:29 +0530

    Added UI to the Book Store using SnackBar from notistack

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -3,12 +3,14 @@ import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { useSnackbar } from "notistack";
 
 const CreateBook = () => {
   const [title,setTitle] = useState('');
   const [author,setAuthor] = useState('');
   const [publishyear,setPublishYear] = useState('');
   const[loading,setLoading] = useState(false);
+  const{ enqueueSnackbar } = useSnackbar();
   const navigate = useNavigate();
   const handleSaveBook = () =>{
     const data = {
@@ -21,11 +23,13 @@ const CreateBook = () => {
     .post('http://localhost:5555/books/create',data)
     .then(() => {
       setLoading(false);
+      enqueueSnackbar('Book created Successfully',{variant:'success'});
       navigate('/');
     })
     .catch((error) =>{
       setLoading(false);
-      alert('An error occured');
+      //alert('An error occured');
+      enqueueSnackbar('Error',{ variant:'error' });
       console.log(error);
     });
   };
--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -3,6 +3,7 @@ import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
+import { useSnackbar } from "notistack";
 
 const EditBook = () => {
   const [title,setTitle] = useState('');
@@ -11,7 +12,7 @@ const EditBook = () => {
   const[loading,setLoading] = useState(false);
   const navigate = useNavigate();
   const {id} = useParams();
-
+  const{ enqueueSnackbar } = useSnackbar();
   useEffect(() =>{
     setLoading(true);
     axios.get(`http://localhost:5555/books/${id}`)
@@ -37,11 +38,13 @@ const EditBook = () => {
     .put(`http://localhost:5555/books/${id}/update`,data)
     .then(() => {
       setLoading(false);
+      enqueueSnackbar('Book Edited Successfully',{variant:'success'});
       navigate('/');
     })
     .catch((error) =>{
       setLoading(false);
-      alert('An error occured');
+      //alert('An error occured');
+      enqueueSnackbar('Error',{variant:'error'});
       console.log(error);
     });
   };
